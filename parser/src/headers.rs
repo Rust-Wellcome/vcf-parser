39eb7f39adfb73bf5cec89f167834d5c91b4a266
Stephen <519327+stevieing@users.noreply.github.com>
2023-04-05 08:32:36 +0100

    Added first 2 tests and is_valid method.

--- parser/src/headers.rs
+++ parser/src/headers.rs
@@ -1,15 +1,29 @@
 
+//! - [ ] Add implementation
+//! - [ ] Check that the first line is fileFormat
+//! - [ ] Check that the file format is a valid version
+pub fn is_valid(headers: String) -> bool {
+  false
+}
+
+/// - [ ] Add docs so no need for implementation
 #[cfg(test)]
 mod tests {
+
+  use super::*;
+    // I have maded the assumption that the headers will already be parsed
+    // and the double hash ## has been removed
     
     #[test]
     fn test_invalid_without_file_format_on_first_line() {
-      panic!("Pending");
+      let header = String::from("fileDate=20090805");
+      assert!(!is_valid(header));
     }
 
     #[test]
     fn test_invalid_without_valid_file_format_version() {
-      panic!("Pending");
+      let header = String::from("##fileformat=VCFx");
+      assert!(!is_valid(header));
     }
 
 }
