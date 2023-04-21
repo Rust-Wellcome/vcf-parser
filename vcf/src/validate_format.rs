a0c65a75f7bf6806f9495a907f7188c5e75fac43
ac <4184070+MrCurtis@users.noreply.github.com>
2023-04-21 21:50:21 +0000

    Check that 'Type' is of one of the allowed values.
    See 1.4.4 of the spec.

--- vcf/src/validate_format.rs
+++ vcf/src/validate_format.rs
@@ -6,7 +6,9 @@ use std::collections::HashSet;
 fn is_valid_format(input: HashMap<&str, &str>) -> bool {
         let keys: HashSet<&str> = input.keys().copied().collect();
         let required_keys = HashSet::from(["ID", "Number", "Type", "Description"]);
-        return required_keys == keys;
+        if !(required_keys == keys) {return false};
+        if !([&"Integer", &"Float", &"Character", &"String"].map(|s| Some(s)).contains(&input.get("Type"))) {return false};
+        return true;
 }
 
 #[cfg(test)]
@@ -91,6 +93,76 @@ mod tests {
         ]);
 
         let result = is_valid_format(extra_key_input);
+
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn returns_true_if_type_is_Integer() {
+        let integer_value_input = HashMap::from([
+            ("ID", "ID123"),
+            ("Number", "3"),
+            ("Type", "Integer"),
+            ("Description", "This is a thing"),
+        ]);
+
+        let result = is_valid_format(integer_value_input);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn returns_true_if_type_is_Float() {
+        let float_value_input = HashMap::from([
+            ("ID", "ID123"),
+            ("Number", "3"),
+            ("Type", "Float"),
+            ("Description", "This is a thing"),
+        ]);
+
+        let result = is_valid_format(float_value_input);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn returns_true_if_type_is_Character() {
+        let character_value_input = HashMap::from([
+            ("ID", "ID123"),
+            ("Number", "3"),
+            ("Type", "Character"),
+            ("Description", "This is a thing"),
+        ]);
+
+        let result = is_valid_format(character_value_input);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn returns_true_if_type_is_String() {
+        let string_value_input = HashMap::from([
+            ("ID", "ID123"),
+            ("Number", "3"),
+            ("Type", "String"),
+            ("Description", "This is a thing"),
+        ]);
+
+        let result = is_valid_format(string_value_input);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn returns_false_if_type_is_not_valid() {
+        let string_value_input = HashMap::from([
+            ("ID", "ID123"),
+            ("Number", "3"),
+            ("Type", "Number"),
+            ("Description", "This is a thing"),
+        ]);
+
+        let result = is_valid_format(string_value_input);
 
         assert_eq!(result, false);
     }
