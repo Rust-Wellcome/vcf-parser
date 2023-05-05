7525dc5bfeeb05e73a61ecbbf26ee4ac905f6e19
jeff-k <jeff_k@fastmail.com>
2023-05-05 19:41:35 +0100

    define structs to describe the format fields and parse from header

--- vcf/src/validate_format.rs
+++ vcf/src/validate_format.rs
@@ -1,12 +1,49 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use regex::Regex;
+use std::str::FromStr;
+
+enum NumberField {
+    Number(u32),
+    A,   // The field has one value per alternate allele
+    R,   // The field has one value for each possible allele
+    G,   // The field has one value for each possible genotype
+    Dot, // The number of possible values varies, is unknown or unbounded
+}
+
+// use static dispatch for Info field parser
+enum DataType {
+    Integer(NumberField),
+    Float(NumberField),
+    Flag,
+    Character(NumberField),
+    String(NumberField),
+}
+
+struct InfoFormat {
+    fieldtype: DataType,
+    description: String,
+    source: Option<String>,
+    version: Option<String>,
+}
+
+fn parse_number_field(number: Option<&&str>) -> Result<NumberField, Box<dyn std::error::Error>> {
+    match number {
+        Some(&"A") => Ok(NumberField::A),
+        Some(&"R") => Ok(NumberField::R),
+        Some(&"G") => Ok(NumberField::G),
+        Some(&".") => Ok(NumberField::Dot),
+        Some(n) => {
+            let num = u32::from_str(n)?;
+            Ok(NumberField::Number(num))
+        }
+        None => Err("Number field not found".into()),
+    }
+}
 
 /// - [X] Check has all and only required keys.
 /// - [ ] Check that values are of the required types.
 fn is_valid_format(input: HashMap<&str, &str>) -> bool {
-    return
-        has_required_keys(&input)
+    return has_required_keys(&input)
         & has_valid_type_value(&input)
         & has_valid_number_value(&input);
 }
@@ -17,20 +54,31 @@ fn has_required_keys(input: &HashMap<&str, &str>) -> bool {
     return required_keys == keys;
 }
 
+fn parse_type_value(
+    info_map: &HashMap<&str, &str>,
+) -> Result<DataType, Box<dyn std::error::Error>> {
+    match info_map.get("Type") {
+        Some(&"Integer") => Ok(DataType::Integer(parse_number_field(
+            info_map.get("Number"),
+        )?)),
+        Some(&"Float") => Ok(DataType::Float(parse_number_field(info_map.get("Number"))?)),
+        Some(&"Flag") => Ok(DataType::Flag),
+        Some(&"Character") => Ok(DataType::Character(parse_number_field(
+            info_map.get("Number"),
+        )?)),
+        Some(&"String") => Ok(DataType::String(parse_number_field(
+            info_map.get("Number"),
+        )?)),
+        _ => Err("Invalid data type".into()),
+    }
+}
+
 fn has_valid_type_value(input: &HashMap<&str, &str>) -> bool {
-    return
-        [&"Integer", &"Float", &"Character", &"String"]
-        .map(|s| Some(s))
-        .contains(&input.get("Type"));
+    parse_type_value(input).is_ok()
 }
 
 fn has_valid_number_value(input: &HashMap<&str, &str>) -> bool {
-    //TODO - Don't compile regex in function.
-    let valid_number_regex = Regex::new(r"^(\d+|G|A|R|\.)$").unwrap();
-    return match input.get("Number") {
-        Some(s) => valid_number_regex.is_match(s),
-        None => false,
-    }
+    parse_number_field(input.get("Number")).is_ok()
 }
 
 #[cfg(test)]
@@ -93,11 +141,8 @@ mod tests {
 
     #[test]
     fn returns_false_if_missing_description() {
-        let missing_description_input = HashMap::from([
-            ("ID", "ID123"),
-            ("Number", "3"),
-            ("Type", "String"),
-        ]);
+        let missing_description_input =
+            HashMap::from([("ID", "ID123"), ("Number", "3"), ("Type", "String")]);
 
         let result = is_valid_format(missing_description_input);
 
