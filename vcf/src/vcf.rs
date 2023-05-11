f365caaf400c203dc86563ed4aa3ddaf92e1d577
ac <4184070+MrCurtis@users.noreply.github.com>
2023-05-11 17:03:36 +0000

    Use actual example from spec in tests.
    This makes sense as this part of the tests should continue to pass as
    each bit of functionality is added.

--- vcf/src/vcf.rs
+++ vcf/src/vcf.rs
@@ -3,7 +3,7 @@
 ///```
 ///use std::fs::File;
 ///use vcf::vcf::parse_vcf;
-///let vcf_file = File::open("example_files/minimal_valid_example.vcf").unwrap();
+///let vcf_file = File::open("example_files/example_1_point_1_from_spec.vcf").unwrap();
 ///let vcf = parse_vcf(vcf_file);
 ///```
 pub fn parse_vcf(vcf_file: std::fs::File) {}
