65e8dbfd61e393972e5dbdcf3008eed5a17c19b5
ac <4184070+MrCurtis@users.noreply.github.com>
2023-06-18 22:52:23 +0000

    Change is_valid_file_format to use refs

--- vcf/src/validate_fileformat.rs
+++ vcf/src/validate_fileformat.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use crate::headers::Header;
 use crate::headers::HeaderValue::{Flat, Nested};
 
-fn is_valid_file_format(input: Header) -> bool {
+pub fn is_valid_file_format(input: &Header) -> bool {
     is_flat(&input)
     & key_is_fileformat(&input)
 }
@@ -26,18 +26,18 @@ mod tests {
     #[test]
     fn is_valid_if_key_is_fileformat() {
         let header = Header {key: "fileformat", value: Flat("VCFv4.4")};
-        assert_eq!(is_valid_file_format(header), true);
+        assert_eq!(is_valid_file_format(&header), true);
     }
 
     #[test]
     fn is_invalid_if_key_is_not_fileformat() {
         let header = Header {key: "gileformat", value: Flat("VCFv4.4")};
-        assert_eq!(is_valid_file_format(header), false);
+        assert_eq!(is_valid_file_format(&header), false);
     }
 
     #[test]
     fn is_invalid_if_header_value_nested() {
         let header = Header {key: "fileformat", value: Nested(HashMap::from([("another_key", "VCFv4.4")])) };
-        assert_eq!(is_valid_file_format(header), false);
+        assert_eq!(is_valid_file_format(&header), false);
     }
 }
