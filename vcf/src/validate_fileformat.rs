8d3a8b70ee8afac238f370cad947ddbbcdb75cb9
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-07-24 13:55:27 +0100

    Tidy up assertions.
    No need to assert equal to true - just assert.
    
    Co-authored-by: Will Burden <25230174+willburden@users.noreply.github.com>

--- vcf/src/validate_fileformat.rs
+++ vcf/src/validate_fileformat.rs
@@ -26,18 +26,18 @@ mod tests {
     #[test]
     fn is_valid_if_key_is_fileformat() {
         let header = Header {key: "fileformat", value: Flat("VCFv4.4")};
-        assert_eq!(is_valid_file_format(&header), true);
+        assert!(is_valid_file_format(&header));
     }
 
     #[test]
     fn is_invalid_if_key_is_not_fileformat() {
         let header = Header {key: "gileformat", value: Flat("VCFv4.4")};
-        assert_eq!(is_valid_file_format(&header), false);
+        assert!(!is_valid_file_format(&header));
     }
 
     #[test]
     fn is_invalid_if_header_value_nested() {
         let header = Header {key: "fileformat", value: Nested(HashMap::from([("another_key", "VCFv4.4")])) };
-        assert_eq!(is_valid_file_format(&header), false);
+        assert!(!is_valid_file_format(&header));
     }
 }
