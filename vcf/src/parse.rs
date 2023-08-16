9f378132a6724b648abb8d2c111cad99a4f768c6
ac <4184070+MrCurtis@users.noreply.github.com>
2023-08-16 12:32:50 +0000

    Add explanation for regex

--- vcf/src/parse.rs
+++ vcf/src/parse.rs
@@ -17,6 +17,9 @@ impl<'src> Header<'src> {
 
 impl<'src> HeaderValue<'src> {
     pub fn parse(input: &'src str) -> Result<Self, ParseError> {
+        // Repeatedly match either non-comma/non-quote characters or blocks of text enclosed in
+        // quotes, until we can't, in which case we're either at a non-quote-enclosed comma or the
+        // end of the string.
         let re = Regex::new(r#"(?:[^,"]+|(?:"[^"]*"))+"#).unwrap();
         match input.strip_prefix('<').and_then(|input| input.strip_suffix('>')) {
             None => Ok(Self::Flat(input)),
